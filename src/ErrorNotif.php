bdfa63120af7128c8c200fdfd5251ca25ff63216
aliaxander <admin@oxcpa.ru>
2015-08-14 11:01:48 +0300

    v1.11 fix for OxApp

--- src/ErrorNotif.php
+++ src/ErrorNotif.php
@@ -13,39 +13,46 @@ namespace Ox;
  * Class ErrorNotif
  * @package OxApp\models
  */
-class ErrorNotif extends AbstractModel {
+class ErrorNotif extends AbstractModel
+{
 
-    public static function build(){
-        echo __DIR__ . "/../errors.php";
-       include_once(__DIR__ . "/../errors.php");
-        $arrayErrors=array();
-        $errorNotif=array();
-        if(!empty($_SESSION['errorNotif'])){
+	public static function build($file = __DIR__ . "/../../../../http/errors.php")
+	{
 
-            $charfix=new charFix();
-            foreach($_SESSION['errorNotif'] as $val) {
+		if (file_exists($file)) {
+			include_once(__DIR__ . "/../../../../http/errors.php");
+			$errorNotif = array();
+			if (!empty($_SESSION['errorNotif'])) {
 
-                    $code = @explode(".", $val['code']);
-                if(!empty($code[0])) {
-                    $title = key($arrayErrors[$code[0]]);
-                    $text = $arrayErrors[$code[0]][$title][$code[1]];
+				$charfix = new charFix();
+				foreach ($_SESSION['errorNotif'] as $val) {
 
-                    if(!empty($val['array'])) {
-                        foreach ($val['array'] as $titleCode => $valCode) {
-                            $text = str_replace(":{$titleCode}", $valCode, $text);
-                        }
-                    }
-                    $errorNotif[] = array("title" => $charfix->FixAll($title,"noSpec|noHtml"), "text" =>$charfix->FixAll($text,"noSpec|noHtml") , "color" => $arrayColor[$code[0]]);
-                }
-            }
-            $_SESSION['errorNotif'] = array();
-        }
+					$code = @explode(".", $val['code']);
+					if (!empty($code[0])) {
+						$title = key($arrayErrors[$code[0]]);
+						$text = $arrayErrors[$code[0]][$title][$code[1]];
 
-        return $errorNotif;
-    }
+						if (!empty($val['array'])) {
+							foreach ($val['array'] as $titleCode => $valCode) {
+								$text = str_replace(":{$titleCode}", $valCode, $text);
+							}
+						}
+						$errorNotif[] = array("title" => $charfix->FixAll($title, "noSpec|noHtml"), "text" => $charfix->FixAll($text, "noSpec|noHtml"), "color" => $arrayColor[$code[0]]);
+					}
+				}
+				$_SESSION['errorNotif'] = array();
+			}
 
-    public static function AddAlert($code,$array=""){
-        if(empty($_SESSION['errorNotif']))$_SESSION['errorNotif']=array();
-        $_SESSION['errorNotif'][]=array("code"=>$code, "array"=>$array);
-    }
+			return $errorNotif;
+		} else {
+			return false;
+		}
+	}
+
+	public static function AddAlert($code, $array = "")
+	{
+		if (empty($_SESSION['errorNotif']))
+			$_SESSION['errorNotif'] = array();
+		$_SESSION['errorNotif'][] = array("code" => $code, "array" => $array);
+	}
 }
