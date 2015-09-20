a9830078834e3278649251054b300c6e5d4f9579
Aliaxander <admin@oxcpa.ru>
2015-09-20 18:56:21 +0300

    Order By

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -24,15 +24,19 @@ abstract class AbstractModel
      */
     public static function findAll()
     {
+        if (!empty($orderBy))
+            $orderBy = array("order" => $orderBy);
         $mysql = new DbMysql();
-        $mysql->cfg = array("table" => static::$table);
+        $mysql->cfg = array("table" => static::$table)+ $orderBy;
         return $mysql->read();
     }
 
-    public static function findByColumn($data)
+    public static function findByColumn($data,$orderBy=array())
     {
+        if(!empty($orderBy))
+            $orderBy=array("order" => $orderBy);
         $mysql = new DbMysql();
-        $mysql->cfg = array("table" => static::$table,"where"=>$data);
+        $mysql->cfg = array("table" => static::$table,"where"=>$data)+ $orderBy;
         return $mysql->read();
     }
     
