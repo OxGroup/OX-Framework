4a4d6a388cc99a3a2316cfb685135c104a0896d7
aliaxander <admin@oxcpa.ru>
2015-10-20 12:36:21 +0300

    fix auth

---
+++ src/Auth.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by OxProfitGroup.
+ * User: aliaxander
+ * Date: 06.06.15
+ * Time: 14:36
+ */
+
+namespace Ox;
+
+use \Symfony\Component\HttpFoundation\Cookie;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+
+class Auth extends AbstractModel
+{
+	public static $table = "users";
+	protected static $userConfig;
+
+	public static function addSession($user)
+	{
+
+		//self::delSession();
+
+		$data = self::getUserConfig($user);
+
+		if (!empty($data->rows[0]->remember_token)) {
+			$newremember_token = $data->rows[0]->remember_token;
+
+		} else {
+
+			$hash = new \Ox\Hash;
+			$newremember_token = $hash->make($data->rows['0']->remember_token . date("H:m:d:Y:s"));
+			self::Update(array("remember_token" => $newremember_token), array("id" => $user));
+
+		}
+
+		$response = new Response();
+		$response->headers->setCookie(new Cookie('id', $user, time() + 60 * 60 * 24 * 30 * 12, "/"));
+		$response->headers->setCookie(new Cookie('username', $data->rows['0']->email, time() + 60 * 60 * 24 * 30 * 12, "/"));
+		$response->headers->setCookie(new Cookie('remember_token', $newremember_token, time() + 60 * 60 * 24 * 30 * 12, "/"));
+		$response->send();
+		return true;
+	}
+
+	public static function delSession()
+	{
+		$response = new Response();
+		$response->headers->clearCookie('id', '');
+		$response->headers->clearCookie('username', '');
+		$response->headers->clearCookie('remember_token', '');
+		$response->send();
+
+		return true;
+	}
+
+	private static function getUserConfig($id = NULL)
+	{
+		return self::findByColumn(array("id" => $id));
+	}
+
+	public static function getStatus()
+	{
+		$cookie = new Request($_COOKIE);
+
+
+		$user = self::findByColumn(array("id" => $cookie->get("id")));
+
+		if (isset($user->rows['0']->remember_token) and isset($user->rows['0']->email) and $user->rows['0']->remember_token == $cookie->get("remember_token") and $user->rows['0']->email == $cookie->get("username")) {
+			self::$userConfig = $user->rows['0'];
+			return true;
+		} else {
+			self::$userConfig = false;
+			return false;
+		}
+	}
+
+	public static function getConfigSess()
+	{
+		$cookie = new Request($_COOKIE);
+
+		$user = self::findByColumn(array("id" => $cookie->get("id")));
+
+		if (isset($user->rows['0']->remember_token) and isset($user->rows['0']->email) and $user->rows['0']->remember_token == $cookie->get("remember_token") and $user->rows['0']->email == $cookie->get("username")) {
+			$user->rows['0']->balance = $user->rows['0']->balance - $user->rows['0']->payd;
+			self::$userConfig = $user->rows['0'];
+			return true;
+		} else {
+			self::$userConfig = false;
+			return false;
+		}
+	}
+
+	public static function GiveMeUserSettings()
+	{
+		$id = self::$userConfig;
+		if (empty($id)) {
+			$id = (object)array();
+			$id->id = 0;
+			$id->name = null;
+		}
+		return $id;
+	}
+
+}
