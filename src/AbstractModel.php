4cafac0a0af9c3a1451e86af762010db7592ca15
Александр <admin@oxcpa.ru>
2015-11-01 16:03:50 +0300

    Abstract cache mysql

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -12,24 +12,59 @@
  * Class AbstractModel
  */
 namespace Ox;
+
 use \Ox\DbMysql;
+
 abstract class AbstractModel
 
 {
     protected static $table;
+    protected static $cache;
+
+
+    public static function getCache($data, $orderBy)
+    {
+        $cache = self::$cache;
+        if (isset($cache[serialize($data)]) and !empty($cache[serialize($data)][serialize($orderBy)])) {
+            return $cache[serialize($data)][serialize($orderBy)];
+        } else {
+            return false;
+        }
+    }
+
+    public static function addCache($data, $orderBy, $result)
+    {
+        $cache = self::$cache;
+        $cache[serialize($data)][serialize($orderBy)] = $result;
+        self::$cache = $cache;
+    }
+
+    public static function clearCache()
+    {
+        self::$cache = array();
+    }
+
 
     /**
      * @param array $orderBy
      *
      * @return object|string|array
      */
-    public static function findAll($orderBy=array())
+    public static function findAll($orderBy = array())
     {
-        if (!empty($orderBy))
-            $orderBy = array("order" => $orderBy);
-        $mysql = new DbMysql();
-        $mysql->cfg = array("table" => static::$table)+ $orderBy;
-        return $mysql->read();
+        $cache = self::getCache("allData", $orderBy);
+        if ($cache == false) {
+            if (!empty($orderBy)){
+                $orderBy = array("order" => $orderBy);
+                }
+            $mysql = new DbMysql();
+            $mysql->cfg = array("table" => static::$table) + $orderBy;
+            $result = $mysql->read();
+            self::addCache("allData", $orderBy, $result);
+            return $result;
+        } else {
+            return $cache;
+        }
     }
 
     /**
@@ -38,13 +73,20 @@ abstract class AbstractModel
      *
      * @return object|string|array
      */
-    public static function findByColumn($data,$orderBy=array())
+    public static function findByColumn($data, $orderBy = array())
     {
-        if(!empty($orderBy))
-            $orderBy=array("order" => $orderBy);
-        $mysql = new DbMysql();
-        $mysql->cfg = array("table" => static::$table,"where"=>$data)+ $orderBy;
-        return $mysql->read();
+        $cache = self::getCache($data, $orderBy);
+        if ($cache == false) {
+            if (!empty($orderBy))
+                $orderBy = array("order" => $orderBy);
+            $mysql = new DbMysql();
+            $mysql->cfg = array("table" => static::$table, "where" => $data) + $orderBy;
+            $result = $mysql->read();
+            self::addCache($data, $orderBy, $result);
+            return $result;
+        } else {
+            return $cache;
+        }
     }
 
     /**
@@ -54,43 +96,56 @@ abstract class AbstractModel
      */
     public static function findByColumnFree($data)
     {
+        $cache = self::getCache("freeData", $data);
+        if ($cache == false) {
         $mysql = new DbMysql();
         $mysql->cfg = array("table" => static::$table);
-        $mysql->freeWhere=$data;
-        return $mysql->read();
+        $mysql->freeWhere = $data;
+            $result = $mysql->read();
+            self::addCache("freeData", $data, $result);
+            return $result;
+        } else {
+            return $cache;
+        }
     }
 
     /**
      * @param $data
+     *
      * @return array|string
      */
     public static function Add($data)
     {
         $mysql = new DbMysql();
         $mysql->cfg = array("table" => static::$table, "data" => $data);
+        self::clearCache();
         return $mysql->create();
     }
 
     /**
      * @param $data
      * @param $where
+     *
      * @return array|string
      */
-    public static function Update($data,$where)
+    public static function Update($data, $where)
     {
         $mysql = new DbMysql();
         $mysql->cfg = array("table" => static::$table, "data" => $data, "where" => $where);
+        self::clearCache();
         return $mysql->update();
     }
 
     /**
      * @param $where
+     *
      * @return array|string
      */
     public static function Delete($where)
     {
         $mysql = new DbMysql();
         $mysql->cfg = array("table" => static::$table, "where" => $where);
+        self::clearCache();
         return $mysql->delete();
     }
 }
