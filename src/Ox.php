9330e7528f45d2f05fe723c021f784eda2e70d52
Александр <admin@oxcpa.ru>
2015-12-04 20:52:36 +0300

    Fix composer

--- src/Ox.php
+++ src/Ox.php
@@ -9,14 +9,16 @@
 namespace Ox;
 
 use OxApp\controllers\Routes;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 class Ox
 {
     public static function start()
     {
 
-        $whoops = new \Whoops\Run;
-        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        $whoops = new Run();
+        $whoops->pushHandler(new PrettyPageHandler());
 
         $whoops->pushHandler(function ($exception, $inspector, $run) {
             $inspector->getFrames()->map(function ($frame) {
